236631802806eff814884c1e266bf7fac49f19e1
Tayyaba Patel <tayyaba.patel@thinkitive.com>
2024-10-01 18:58:40 +0530

    feat/reactDay7

---
+++ Module2/Day7/FormLibrary/src/App.jsx
@@ -0,0 +1,15 @@
+import YouTubeForm from "./Components/YouTubeForm"
+// import styles from "./Components/youtubeform.module.css";
+
+function App() {
+
+  return (
+    <>
+    <YouTubeForm/>
+
+    </>
+   
+  )
+}
+
+export default App
